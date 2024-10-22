11edb674cd4b1b9749712cb13f8b473de7f65936
pablo <u22220256@utp.edu.pe>
2024-10-22 15:52:04 -0500

    Auto-generated commit

--- chunk-3QZAC23P.js
+++
@@ -1,1 +0,0 @@
-import{a as t,c as i,j as n,o as m}from"./chunk-DP33OLTD.js";var u=[],e=class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[m.forChild(u),m]})};var c=class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[n,e]})};export{c as InicioModule};
